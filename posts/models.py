dab26cec6ab4d3f84c7509dcf7a52079802f4c75
Melissa Reyes <mreyes1563@conestogac.on.ca>
2025-03-31 19:34:34 -0400

    Fixed typos

--- posts/models.py
+++ posts/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from profiles.models import Profile
-
 # Create your models here.
-
 class Post(models.Model):
-    title = models.CharField(max_lenght=200)
+    title = models.CharField(max_length=200)
     body = models.TextField()
     liked = models.ManyToManyField(User, blank=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def__str__(self):
+    def str(self):
         return str(self.title)
--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(balnk=True)
+    bio = models.TextField(blank=True)
     avatar = models.ImageField(default='avatar.png', upload_to='avatars')
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
