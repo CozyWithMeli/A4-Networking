c733bf96f42534723fa47b80d6365d6eb1b5f917
Melissa Reyes <mreyes1563@conestogac.on.ca>
2025-04-01 15:52:54 -0400

    Added a console log from ajax

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,4 +2,28 @@ console.log('hello world')
 
 const helloWolrdBox = document.getElementById('hello-world')
 
-helloWolrdBox.innerHTML='hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWolrdBox.textContent=response.text
+    },
+    error: function(error){
+    console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url:'/data/',
+    success: function (response){
+        console.log(response)
+        const data = JSON.parse(response.data)
+        console.log(data)
+    },
+    error: function(){
+        console.log(error)
+    }
+})
