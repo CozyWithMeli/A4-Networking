34b9545ba24834bf31e19e43c62fad53b3e8c8bd
Melissa Reyes <mreyes1563@conestogac.on.ca>
2025-04-02 18:59:16 -0400

    Added crispy and install boostrap5 since bootstrap4 no longer works

--- posts/views.py
+++ posts/views.py
@@ -1,34 +1,53 @@
+from pydoc import visiblename
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from .forms import PostForm
+from profiles.models import Profile
+
 # Create your views here.
 
+
 def post_list_and_create(request):
-    qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs})
+    form = PostForm(request.POST or None)
+    # qs = Post.objects.all()
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.author = author
+            instance.save()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/main.html', context)
+
 
 def load_post_data_view(request, num_posts):
     visible = 3
     upper = num_posts
     lower = upper - visible
     size = Post.objects.all().count()
-    
+
     qs = Post.objects.all()
     data = []
     for obj in qs:
         item = {
-          'id':obj.id,
-          'title':obj.title,
-          'body':obj.body,
-          'liked': True if request.user in obj.liked.all() else False,
-          'count': obj.like_count,
-          'author':obj.author.user.username
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'liked': True if request.user in obj.liked.all() else False,
+            'count': obj.like_count,
+            'author': obj.author.user.username
         }
+
         data.append(item)
-    return JsonResponse ({'data':data[lower:upper], 'size':size})
+    return JsonResponse({'data': data[lower:upper], 'size': size})
+
 
 def like_unlike_post(request):
-     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         pk = request.POST.get('pk')
         obj = Post.objects.get(pk=pk)
         if request.user in obj.liked.all():
@@ -39,5 +58,6 @@ def like_unlike_post(request):
             obj.liked.add(request.user)
         return JsonResponse({'liked': liked, 'count': obj.like_count})
 
+
 def hello_world_view(request):
-    return JsonResponse({'text': 'hello worldx2'})
+    return JsonResponse({'text': 'hello worldX2'})
