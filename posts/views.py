2a3d750bdcf4fb8924a3e23c3f24e6ce6d5e2425
Melissa Reyes <mreyes1563@conestogac.on.ca>
2025-04-04 15:59:41 -0400

    Added image_upload_view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from pydoc import visiblename
 from django.shortcuts import render
 from .models import Post
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 
@@ -108,3 +108,11 @@ def delete_post(request, pk):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         obj.delete()
     return JsonResponse({})
+
+def image_upload_view(request):
+    if request.method == 'POST':
+        img = request.FILES.get('flie')
+        new_post_id = request.POST.get('new_post_id')
+        post = Post.objects.get(id=new_post_id)
+        Photo.objects.create(image=img, post=post)
+    return HttpResponse()
