465fe0becc163f20546299dea121ce75ee34ca43
Melissa Reyes <mreyes1563@conestogac.on.ca>
2025-04-04 15:17:37 -0400

    Added corousel and with controls and added photo object

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return str(self.title)
@@ -18,5 +19,16 @@ class Post(models.Model):
     def like_count(self):
         return self.liked.all().count()
     
+    def get_photos(self):
+        return self.photo_set.all()
+    
     class Meta:
         ordering = ("-created",)
+        
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="posts/")
+    created = models.DateTimeField(auto_now_add=True)
+        
+    def __str__(self):
+        return f"{self.post.title} - {self.pk}"
